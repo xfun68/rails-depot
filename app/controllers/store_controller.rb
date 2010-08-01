309f3d44da0587b0f7aa630aa5764c2c04912cb3
xfun68 <xfun68@gmail.com>
2010-08-01 17:41:32 +0800

    去掉 redirect_to index 引进的重复代码

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -10,20 +10,23 @@ class StoreController < ApplicationController
     @cart.add_product(product)
   rescue ActiveRecord::RecordNotFound
     logger.error("Attempt to access invalid product #{params[:id]}")
-    flash[:notice] = "Invalid product"
-    redirect_to :action => 'index'
+    redirect_to_index("Invalid product")
   end
 
   def empty_cart
     session[:cart] = nil
-    flash[:notice] = "Your cart is currently empty"
-    redirect_to :action => 'index'
+    redirect_to_index("Your cart is currently empty")
   end
 
   private
 
   def find_cart
     session[:cart] ||= Cart.new
+  end
+
+  def redirect_to_index(msg)
+    flash[:notice] = msg
+    redirect_to :action => 'index'
   end
 
 end
