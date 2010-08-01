cf240e50c829b26b7ec01a8dcaf8cc8831665b4c
xfun68 <xfun68@gmail.com>
2010-08-01 16:23:38 +0800

    增加session的设置

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,11 @@
 class StoreController < ApplicationController
+
   def index
     @products = Product.find_products_for_sale
+  end
+
+  def find_cart
+    session[:cart] ||= Cart.new
   end
 
 end
